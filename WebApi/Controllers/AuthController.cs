99733f068fd384ed286049b8f4646021cc5fe197
DarkKezar <darkkezaring@gmail.com>
2022-11-27 19:39:18 +0300

    Fourth Step
    1. Add auth by JWT-token

---
+++ WebApi/Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : Controller
+{
+    private readonly IConfiguration _configuration;
+    private readonly UserManager<User> _userManager;
+
+    public AuthController(IConfiguration configuration, UserManager<User> userManager)
+    {
+        _configuration = configuration;
+        _userManager = userManager;
+    }
+    
+    [Route("login")]
+    [HttpPost]
+    public async Task<IResult> AuthAsync(string email, string password)
+    {
+        
+        
+        if (await _userManager.CheckPasswordAsync((await _userManager.FindByEmailAsync(email)), password))
+        {
+            var issuer = _configuration.GetValue<string>("Jwt:Issuer");
+            var audience = _configuration.GetValue<string>("Jwt:Audience");
+            var key = Encoding.ASCII.GetBytes(_configuration.GetValue<string>("Jwt:Key"));
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("Id", Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Sub, email),
+                    new Claim(JwtRegisteredClaimNames.Email, email),
+                    new Claim(JwtRegisteredClaimNames.Jti,
+                        Guid.NewGuid().ToString())
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(60),
+                Issuer = issuer,
+                Audience = audience,
+                SigningCredentials = new SigningCredentials
+                (new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha512Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var jwtToken = tokenHandler.WriteToken(token);
+            var stringToken = tokenHandler.WriteToken(token);
+            return Results.Ok(stringToken);
+        }
+        return Results.Unauthorized();
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,14 +1,51 @@
+using System.Text;
+using Core.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.IdentityModel.Tokens;
+using AppContext = Core.Context.AppContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//builder.Services.TryAddScoped<UserManager<User>>();
 // Add services to the container.
+builder.Services
+    .AddDbContext<AppContext>
+        (options => options.UseNpgsql(builder.Configuration["ConnectionStrings:DefaultConnection"]));
+builder.Services.AddIdentity<User, Role>().AddEntityFrameworkStores<AppContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey
+            (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = true
+    };
+});
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -23,3 +60,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
